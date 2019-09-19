a449995649ad31a8bf0aa1a0003b4501bdd830fa
Natália <natalia.gama.mattos@hotmail.com>
2019-09-19 10:54:24 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void hanoi_tower(int disco, char comeco, char meio, char fim){
+  if(disco == 1){
+    printf("\nMover disco 1 do %c  para o %c",comeco,fim);
+  }
+  else{
+    hanoi_tower(disco-1, comeco, fim, meio);
+    printf("\nMover disco %d do %c para o %c",disco,comeco,fim);
+    hanoi_tower(disco-1,meio, comeco, fim);//não entendo o pq disso (=^.^=)
+  }
+}
+int main(){
+     int n;
+     printf("\n~~~~Hanoi Tower~~~~\n");
+     printf("Digite o númerod de discos : ");
+     scanf("%d",&n);
+     hanoi_tower(n,'C','M','F');
+}
+
+//temos que fazer o código de forma dinâmica!
+//eu fiz o código com ajuda da Internet
+//tem uma parte dele que eu não to conseguindo entender, aliás eu tenho serios problemas com recursão x.x       
+
+
